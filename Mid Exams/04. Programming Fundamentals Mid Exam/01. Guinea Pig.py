8a6041b60d1e8d92e20bda6de698e40204947c99
simonen <si.petrov@yahoo.com>
2023-04-23 22:15:07 +0300

    initial

--- Mid Exams/04. Programming Fundamentals Mid Exam/01. Guinea Pig.py
+++ Mid Exams/04. Programming Fundamentals Mid Exam/01. Guinea Pig.py
@@ -9,7 +9,7 @@ for day in range(1, 31):
         hay -= food * 0.05
     if day % 3 == 0:
         cover -= pig_weight / 3
-    if min([round(food, 2), round(hay, 2), round(cover, 2)]) <= 0:
+    if min(list(map(lambda x: round(x, 2), [food, hay, cover]))) <= 0:
         print("Merry must go to the pet store!")
         break
 else:
