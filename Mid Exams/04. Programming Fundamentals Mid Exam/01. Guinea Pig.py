677e258214e060591940e15089d2702a9fdf1784
simonen <si.petrov@yahoo.com>
2023-04-23 22:12:56 +0300

    initial

---
+++ Mid Exams/04. Programming Fundamentals Mid Exam/01. Guinea Pig.py
@@ -0,0 +1,16 @@
+food = float(input())
+hay = float(input())
+cover = float(input())
+pig_weight = float(input())
+
+for day in range(1, 31):
+    food -= 0.3
+    if day % 2 == 0:
+        hay -= food * 0.05
+    if day % 3 == 0:
+        cover -= pig_weight / 3
+    if min([round(food, 2), round(hay, 2), round(cover, 2)]) <= 0:
+        print("Merry must go to the pet store!")
+        break
+else:
+    print(f"Everything is fine! Puppy is happy! Food: {food:.2f}, Hay: {hay:.2f}, Cover: {cover:.2f}.")
