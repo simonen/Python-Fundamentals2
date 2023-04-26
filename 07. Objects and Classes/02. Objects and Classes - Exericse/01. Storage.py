c10a7b23ff4203f3acbe9a4a58923c4517236a61
simonen <si.petrov@yahoo.com>
2023-04-26 23:16:55 +0300

    initial

---
+++ 07. Objects and Classes/02. Objects and Classes - Exericse/01. Storage.py
@@ -0,0 +1,21 @@
+class Storage:
+
+    def __init__(self, capacity: int):
+        self.storage = []
+        self.capacity = capacity
+
+    def add_product(self, product: str):
+        if len(self.storage) < self.capacity:
+            self.storage.append(product)
+
+    def get_products(self):
+        return self.storage
+
+
+storage = Storage(4)
+storage.add_product("apple")
+storage.add_product("banana")
+storage.add_product("potato")
+storage.add_product("tomato")
+storage.add_product("bread")
+print(storage.get_products())
