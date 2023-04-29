5cc6fc883cb8355e7f9cee51183a3c851fb09dbc
simonen <si.petrov@yahoo.com>
2023-04-29 21:49:33 +0300

    initial

---
+++ 08. Dictionaries/01. Dictionaries - Lab/06. Odd Occurrences.py
@@ -0,0 +1,13 @@
+line = input().split()
+
+elements = {}
+
+for i in line:
+    key = i.lower()
+    if key not in elements:
+        elements[key] = 0
+    elements[key] += 1
+
+
+odds = [x for x in elements if elements[x] % 2 != 0 ]
+print(" ".join(odds))
