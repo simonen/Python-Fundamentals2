b15f3ee50e302732242b82faa090db4db86cd80e
simonen <si.petrov@yahoo.com>
2023-05-01 00:48:10 +0300

    initial

--- Dictionary Inversion.py
+++ Dictionary Inversion.py
@@ -1,6 +1,6 @@
 def dict_invert(book_f: dict):
-    keys = [x for x in book_f.keys()]
-    values = [x for x in book_f.values()]
+    keys = list(book_f.keys())
+    values = list(book_f.values())
     new_book = {}
     for i in range(len(values)):
         if values[i] not in new_book:
@@ -10,6 +10,7 @@ def dict_invert(book_f: dict):
     return new_book
 
 
-book = {'Royal': 'Lighter', 'Ivan Ivanov': 'Lighter', 'DCay': 'Lighter', 'Palqk': 'Red'}
-
-print(dict_invert(book))
+book = {'Royal': 'Lighter', 'Ivan Ivanov': 'Red', 'DCay': 'Lighter', 'Palqk': 'Red'}
+print(book)
+newbook = dict_invert(book)
+print(newbook)
