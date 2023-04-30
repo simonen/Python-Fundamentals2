9d060dd37339ec4fbab5a5960c096c1528c8db90
simonen <si.petrov@yahoo.com>
2023-04-30 22:04:23 +0300

    initial

---
+++ 08. Dictionaries/02. Dictionaries - Exercise/11. Force Book.py
@@ -0,0 +1,28 @@
+command = input()
+book = {}
+
+while command != "Lumpawaroo":
+    is_user = False
+    if " | " in command:
+        command = command.split(" | ")
+        force_side, force_user = command[0], command[1]
+        if force_user not in book:
+            book[force_user] = force_side
+    elif " -> " in command:
+        command = command.split(" -> ")
+        force_user, force_side = command[0], command[1]
+        if force_user in book:
+            del(book[force_user])
+        book[force_user] = force_side
+        print(f"{force_user} joins the {force_side} side!")
+
+    command = input()
+
+members = [x for x in book.values()]
+sides = []
+for i in members:
+    if i not in sides:
+        sides.append(i)
+        users = "\n! ".join([x for x in book if book[x] == i])
+        print(f"Side: {i}, Members: {members.count(i)}")
+        print(f"! {users}")
