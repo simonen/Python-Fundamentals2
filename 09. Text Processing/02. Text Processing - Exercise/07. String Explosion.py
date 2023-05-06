db8e4186cac829014c4ffb35bc99c034989c1ae9
simonen <si.petrov@yahoo.com>
2023-05-06 21:56:19 +0300

    initial

--- 09. Text Processing/02. Text Processing - Exercise/07. String Explosion.py
+++ 09. Text Processing/02. Text Processing - Exercise/07. String Explosion.py
@@ -1,11 +1,26 @@
-string = input().split(">")
-explosion = 0
-for i, word in enumerate(string):
-    if word[0].isdigit():
-        boom = int(word[0])
-        explosion += boom
-        string[i] = word[explosion:]
-        exploded = (len(word) - len(string[i]))
-        explosion -= exploded
+# string = input().split(">")
+# explosion = 0
+# for i, word in enumerate(string):
+#     if word[0].isdigit():
+#         boom = int(word[0])
+#         explosion += boom
+#         string[i] = word[explosion:]
+#         exploded = (len(word) - len(string[i]))
+#         explosion -= exploded
+#
+# print(">".join(string))
+#
+string = input()
 
-print(">".join(string))
+power = 0
+word = ""
+
+for char in string:
+    if char.isdigit():
+        power += int(char)
+    if power == 0 or char == ">":
+        word += char
+    else:
+        power -= 1
+
+print(word)
